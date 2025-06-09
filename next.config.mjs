028bb922752c02346180e66109410a4edfdef9e7
sobuj21 <sobuj21@users.noreply.github.com>
2025-06-09 14:58:20 -0500

    Add basePath for GitHub Pages styling

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').Config} */
+
+const repo = 'aurtho-store'; // This is the name of your repository
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}/`;
+
 const config = {
-  output: 'export', // Add this line
-  // ... some other settings might be here
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 export default config;
