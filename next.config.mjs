4732e8220b56a1a0239556b9925618f00188f3f8
sobuj21 <sobuj21@users.noreply.github.com>
2025-06-09 15:11:20 -0500

    Fix: Remove trailing slash from basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').Config} */
 
 const repo = 'aurtho-store'; // This is the name of your repository
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}/`;
+const assetPrefix = `/${repo}/`; // assetPrefix needs the trailing slash
+const basePath = `/${repo}`;    // basePath must NOT have a trailing slash
 
 const config = {
   output: 'export',
